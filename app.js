90357d973e40115b8d26cfb9933000e054109330
ruzio <carlo.9942@gmail.com>
2022-06-08 16:18:20 -0400

    se realiza la captura de datos y queda como json

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const botonn = document.getElementById('btn-enviar');
+// const botonn = document.getElementById('btn-enviar');
 
-function llamar(){
-    console.log('click');
-};
+// function llamar(){
+//     console.log('click');
+// };
 
-botonn.addEventListener("click", llamar, false);
+// botonn.addEventListener("click", llamar, false);
+
+document.querySelector('form').addEventListener('submit', e => {
+    e.preventDefault();
+    const data = Object.fromEntries(new FormData(e.target));
+    alert(JSON.stringify(data));
+});
