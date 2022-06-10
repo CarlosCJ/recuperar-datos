26d89be88db63ba304a992c523b9f61fd543ff5f
ruzio <carlo.9942@gmail.com>
2022-06-09 22:12:27 -0400

    Se realizan pruebas para almacenar info en array

--- app.js
+++ app.js
@@ -5,9 +5,25 @@
 // };
 
 // botonn.addEventListener("click", llamar, false);
+let datos = [];
 
-document.querySelector('form').addEventListener('submit', e => {
-    e.preventDefault();
-    const data = Object.fromEntries(new FormData(e.target));
-    alert(JSON.stringify(data));
-});
+// document.querySelector('form').addEventListener('submit', e => {
+//     e.preventDefault();
+//     const data = JSON.stringify(Object.fromEntries(new FormData(e.target)));
+//     //alert(data);
+//     datos.push(data)
+// });
+
+function recuperarDatos(){
+    document.querySelector('form').addEventListener('submit', e => {
+        e.preventDefault();
+        let data = JSON.stringify(Object.fromEntries(new FormData(e.target)));
+        return data;
+    });
+}
+// function addObj(obj){
+//     console.log(obj);
+// }
+
+let obj = recuperarDatos();
+console.log(obj)
